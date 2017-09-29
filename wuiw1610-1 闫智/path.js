41085046b3833a41961aed7ffb6e6e875ea858e4
uncle-Yan <846656730@qq.com>
2017-09-29 16:34:33 +0800

    path第一次

---
+++ wuiw1610-1 闫智/path.js
@@ -0,0 +1,32 @@
+var path = require('path');
+// console.log(path.sep)
+
+// var p = "d://sd//efdc//f.js";
+// console.log(path.normalize(p));
+
+// var p = "d:sd//sdsd//ss";
+// var p1 = "../../aa//yyy";
+// console.log(path.join(p,p1));
+
+
+// var p = __filename;
+// var p1 = "raed.js";
+// console.log(path.resolve(p,p1));
+
+// var p = "sd/sd/www";
+// console.log(path.isAbsolute(p))
+
+// var p = "F:\\nodejs\\path.js";
+// var p1 = "F:\\nodejs\\path\\relative.js"
+// console.log(path.relative(p,p1))
+
+// console.log(path.dirname(p))
+
+// console.log(path.basename(p));
+
+// console.log(path.extname(p));
+
+
+// var p = "d:sd/rr/p.js";
+// console.log(path.parse(p));
+
