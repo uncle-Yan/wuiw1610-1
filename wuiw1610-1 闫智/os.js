5f8644aba786644474a7fd12158c5e68d1e8ba83
uncle-Yan <846656730@qq.com>
2017-09-30 10:15:05 +0800

    闫智 os

---
+++ wuiw1610-1 闫智/os.js
@@ -0,0 +1,23 @@
+const os = require('os');
+
+// console.log(os.cpus());
+
+// console.log(os.endianness());
+
+// console.log(os.hostname());
+
+// console.log(os.networkInterfaces());
+
+// console.log(os.uptime());
+
+// console.log(os.type());
+
+// console.log(os.totalmem()/(1024*1024*1024));
+
+// console.log(os.release());
+
+// console.log(os.tmpdir())
+
+// console.log(os.homedir());
+
+// console.log(os.arch());
